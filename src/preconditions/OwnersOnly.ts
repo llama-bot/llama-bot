99450845de8bb437a019a4ce3771a875c2605763
developomp <developomp@gmail.com>
2021-09-21 12:17:45 +0900

    converted precondition class to function

---
+++ src/preconditions/OwnersOnly.ts
@@ -0,0 +1,18 @@
+import { createFunctionPrecondition } from "@sapphire/decorators"
+import { SapphireClient } from "@sapphire/framework"
+import { Message, MessageEmbed } from "discord.js"
+
+import type { Snowflake } from "discord-api-types"
+
+export default function ownersOnly() {
+	// IDs of users who can run owners only commands
+	// convert comma separated string to array and remove empty values
+	// trailing comma and double comma can result in empty values
+	const owners: Snowflake[] = process.env.OWNER_IDS.split(",").filter(
+		(elem) => elem
+	)
+
+	return createFunctionPrecondition((message: Message): boolean =>
+		owners.includes(message.author.id)
+	)
+}
