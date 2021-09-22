f1809f3a74eb606a58d7d3109d3a7e4b74d4af47
developomp <developomp@gmail.com>
2021-09-22 14:24:23 +0900

    removed auto message send

--- src/util/isChannelNSFW.ts
+++ src/util/isChannelNSFW.ts
@@ -1,17 +1,5 @@
 import { Message, MessageEmbed } from "discord.js"
 
 export default function (message: Message): boolean {
-	const isChannelNSFW = Reflect.get(message.channel, "nsfw") === true
-
-	if (!isChannelNSFW) {
-		message.channel.send({
-			embeds: [
-				new MessageEmbed()
-					.setTitle("Error!")
-					.setDescription("You cannot run this command outside NSFW channels."),
-			],
-		})
-	}
-
-	return isChannelNSFW
+	return Reflect.get(message.channel, "nsfw") === true
 }
