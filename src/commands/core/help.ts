c52cea4852a5a07a7ffcf5f5e2c1d78fbf35835a
developomp <developomp@gmail.com>
2021-12-02 16:02:23 +0900

    removed long comment containing old python code, cleaned code, improved help message readability, and fixed help command incorrectly formatting strings.

--- src/commands/core/help.ts
+++ src/commands/core/help.ts
@@ -88,14 +88,17 @@ Shows info about \`ping\` command:
 	}
 
 	sendDefaultHelpMessage(message: Message): void {
-		const helpEmbed = new MessageEmbed()
-			.setTitle("Help")
-			.setDescription(
-				`Type \`${process.env.PREFIX}help <command | category>\` to get more information about a command or a command category.`
-			)
+		const helpEmbed = new MessageEmbed().setTitle("Help").setDescription(
+			`Use \`${process.env.PREFIX}help <command | category>\` command to get more information about a command or a command category.
+This command is not case sensitive.`
+		)
 
-		this.lowerCaseCategoryNames.forEach((elem) =>
-			helpEmbed.addField(elem, "this command category is for something.", true)
+		helpEmbed.addField(
+			"Categories",
+			this.lowerCaseCategoryNames
+				.map((categoryName) => `- \`${categoryName}\`\n`)
+				.join(""),
+			true
 		)
 
 		message.channel.send({
@@ -120,28 +123,32 @@ Shows info about \`ping\` command:
 	}
 
 	sendCategoryHelpMessage(message: Message, query: string): void {
-		const commandsStore = this.container.stores.get("commands")
 		let selectedCategoryName = ""
 
-		commandsStore.categories.map((categoryName) => {
+		this.commands.categories.map((categoryName) => {
 			if (categoryName.toLowerCase() === query.toLowerCase())
 				selectedCategoryName = categoryName
 		})
 
 		if (!selectedCategoryName) return
 
-		const commandsInCategory = commandsStore.filter((command) =>
+		const commandsInCategory = this.commands.filter((command) =>
 			command.fullCategory.includes(selectedCategoryName)
 		)
 
 		message.channel.send({
 			embeds: [
 				new MessageEmbed()
-					.setTitle(selectedCategoryName)
-					.setDescription("Category description")
+					.setTitle(`${selectedCategoryName} commands`)
+					.setDescription(
+						`Use the \`${process.env.PREFIX}help <command>\` command to get more information about a command.
+This command is not case sensitive.`
+					)
 					.addField(
 						"commands",
-						commandsInCategory.map((command) => `-${command.name}\n`).join("")
+						commandsInCategory
+							.map((command) => `-\`${command.name}\`\n`)
+							.join("")
 					),
 			],
 		})
@@ -162,97 +169,11 @@ Shows info about \`ping\` command:
 
 		message.channel.send({
 			embeds: [
-				new MessageEmbed()
-					.setTitle("Command not found")
-					.setDescription(
-						`Command \`${query}\` was not found. Did you mean \`${mostLikelyGuess}\`?`
-					),
+				new MessageEmbed().setTitle("Command not found").setDescription(
+					`Command \`${query}\` was not found. Did you mean \`${mostLikelyGuess}\`?
+You can also use the \`${process.env.PREFIX}help\` command to list all available commands.`
+				),
 			],
 		})
 	}
 }
-
-/*
-    async def help(self, ctx, cog_str=None):
-        if not cog_str:  # when no argument is passed
-            help_embed = discord.Embed(
-                title="Help",
-                description=f"Use `{self.bot.command_prefix}help <cog>` command to get more information on a cog. (case insensitive)",
-            )
-
-            for cog_name in self.bot.cogs:
-                # Show command to get help for a cog
-                help_embed.add_field(
-                    name=cog_name,
-                    value=f"`{self.bot.command_prefix}help {cog_name.lower()}`",
-                )
-
-            for cog_name in self.bot.cogs:
-                # fields that will be shown in main help embed
-                try:
-                    for field in self.bot.get_cog(cog_name).main_help_fields:
-                        help_embed.add_field(
-                            name=field[0], value=field[1], inline=False
-                        )
-                except AttributeError:
-                    # cog doesn't have main_help_fields
-                    pass
-
-            await ctx.send(embed=help_embed)
-        else:  # when searching for a specific cog
-            cogs = list(self.bot.cogs.keys())
-            lower_cogs = [cog.lower() for cog in cogs]
-            lower_cog_str = cog_str.lower()  # lower case string of cog
-
-            if lower_cog_str in lower_cogs:  # when a cog has been found
-                cog = self.bot.get_cog(cogs[lower_cogs.index(lower_cog_str)])
-
-                await ctx.send(
-                    embed=discord.Embed(
-                        title=f'"{lower_cog_str}" commands',
-                        description=f"Use `{self.bot.command_prefix}help <command>` to get more information about a command. (case insensitive)\n\n"
-                        # keep cog_help empty if cog.help_msg does not exist
-                        + (cog.help_msg + ("\n\n" if cog.help_msg else ""))
-                        if hasattr(cog, "help_msg")
-                        else ""
-                        + f"**Commands:**\n `{'`, `'.join([command.name for command in cog.get_commands()])}`",
-                    )
-                )
-            else:  # when a cog has not been found
-                command: discord.ext.commands.Command = discord.utils.get(
-                    self.bot.commands, name=cog_str
-                )
-
-                if not command:  # when command was not found
-                    await ctx.send(
-                        embed=discord.Embed(
-                            description=f"Cannot find cog/command **{cog_str}**.\nUse the `{self.bot.command_prefix}help` command to list all enabled cogs."
-                        )
-                    )
-                else:  # when a command has been found
-                    await ctx.send(
-                        embed=discord.Embed(
-                            title=f"{self.bot.command_prefix}{command.name}",
-                            description=(
-                                f"**Aliases:** `{'`, `'.join(command.aliases)}`"
-                                if command.aliases
-                                else ""
-                            )
-                            + "\n\n**Description:**\n"
-                            + (
-                                command.help
-                                if command.help
-                                else "Under construction..."
-                            )
-                            + "\n\n**Usage:**\n"
-                            + (
-                                command.usage
-                                if command.usage
-                                else "> {prefix}{command}"
-                            ).format(
-                                prefix=self.bot.command_prefix, command=command.name
-                            ),
-                        )
-                    )
-
-*/
