02ce96a2206fb12c3c4331f5f65ad039889c163d
developomp <developomp@gmail.com>
2022-02-21 22:00:42 +0900

    Improved `clapify` command - Improved command description   - Fixed capitalization   - Clapified command description - Simplified embeded message building - Replaced `:clap:` with acual clap emoji (👏)

--- src/commands/fun/clapify.ts
+++ src/commands/fun/clapify.ts
@@ -5,31 +5,40 @@ import { Formatters, Message, MessageEmbed } from "discord.js"
 @ApplyOptions<CommandOptions>({
 	aliases: ["c", "clap"],
 	description:
-		"does the annoying Karen clap. Does not work with external emojis.",
+		"Does the annoying Karen clap.👏Does👏not👏work👏with👏external👏emojis.",
 })
 export default class ClapifyCommand extends Command {
 	async messageRun(message: Message, args: Args): Promise<void> {
 		const inputs = await args.repeat("string").catch(() => [])
 
+		//
+		// Handle empty argument
+		//
+
 		if (!inputs) {
 			message.channel.send({
 				embeds: [
-					new MessageEmbed()
-						.setTitle("Error!")
-						.setDescription("What should I clapify?"),
+					new MessageEmbed({
+						title: "Error!",
+						description: "What should I clapify?",
+					}),
 				],
 			})
 
 			return
 		}
 
+		//
+		// Reply
+		//
+
 		message.channel.send({
 			embeds: [
-				new MessageEmbed().setDescription(`**${Formatters.userMention(
-					message.author.id
-				)} says:**
+				new MessageEmbed({
+					description: `**${Formatters.userMention(message.author.id)} says:**
 
-${inputs.join(":clap:")}`),
+${inputs.join("👏")}`,
+				}),
 			],
 		})
 	}
