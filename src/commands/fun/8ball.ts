7c559133fdca7c53e3411169d824c79407ff21be
developomp <developomp@gmail.com>
2022-02-21 21:41:49 +0900

    improved 8ball command - Improved command description - Fixed user input getting parsed as an array of character instead   of an array of string - Removed usage of deprecated function - Simplified embeded message building

--- src/commands/fun/8ball.ts
+++ src/commands/fun/8ball.ts
@@ -7,24 +7,46 @@ import { globalObject } from "../.."
 @ApplyOptions<CommandOptions>({
 	aliases: ["8", "ball"],
 	description:
-		"Gives you the best advice. We are not responsible for your action.",
+		"Gives you the best advice you can get. We are not responsible for your action though.",
 })
 export default class EightBallCommand extends Command {
 	async messageRun(message: Message, args: Args) {
 		message.channel.sendTyping()
 
-		const input = [...(await args.repeat("string").catch(() => ""))].join(" ")
+		//
+		// Parse user input
+		//
 
-		const response = await globalObject.nekosClient.sfw["eightBall"]({
-			text: input,
+		let text = ""
+
+		const allStr = await args.repeat("string").catch(() => "")
+
+		if (typeof allStr === "string") {
+			text = allStr
+		}
+		if (Array.isArray(allStr)) {
+			text = allStr.join(" ")
+		}
+
+		//
+		// Get response from nekos.life
+		//
+
+		const { response, url } = await globalObject.nekosClient.sfw["eightBall"]({
+			text,
 		})
+
+		//
+		// Reply
+		//
 
 		message.channel.send({
 			embeds: [
-				new MessageEmbed()
-					.setTitle(response.response)
-					.setImage(response.url || "")
-					.setFooter("powered by nekos.life"),
+				new MessageEmbed({
+					title: response,
+					image: { url },
+					footer: { text: "powered by nekos.life" },
+				}),
 			],
 		})
 	}
