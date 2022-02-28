3dade3e7263d8337c1f921325e9a50ee7c7787a2
developomp <developomp@gmail.com>
2022-02-28 12:42:53 +0900

    Improved snowflake command - improved embeded message building - added date delta

--- src/commands/util/snowflake.ts
+++ src/commands/util/snowflake.ts
@@ -2,7 +2,7 @@ import { SnowflakeUtil, Message, MessageEmbed } from "discord.js"
 import { Args, Command, CommandOptions } from "@sapphire/framework"
 import { ApplyOptions } from "@sapphire/decorators"
 
-import { formatDate, timeDiff } from "../../util"
+import { countDays, formatDate, timeDiff } from "../../util"
 
 @ApplyOptions<CommandOptions>({
 	aliases: ["s"],
@@ -16,35 +16,48 @@ export default class SnowflakeCommand extends Command {
 
 		try {
 			input = await args.pick("string")
-		} catch (e) {
+		} catch {
 			message.channel.send({
 				embeds: [
-					new MessageEmbed()
-						.setTitle("Error")
-						.setDescription("You did not pass any snowflake to parse :("),
+					new MessageEmbed({
+						title: "Error",
+						description: "You did not pass any snowflake :(",
+					}),
 				],
 			})
+
 			return
 		}
 
 		try {
 			const now = message.editedTimestamp || message.createdTimestamp
 			const creationDate = SnowflakeUtil.deconstruct(input).date
+			const dateDelta = countDays(creationDate.getTime(), now)
 
 			message.channel.send({
 				embeds: [
-					new MessageEmbed()
-						.setTitle(input)
-						.addField("Creation Date (UTC)", formatDate(creationDate))
-						.addField("Age", timeDiff(creationDate.getTime(), now)),
+					new MessageEmbed({
+						title: input,
+						fields: [
+							{
+								name: "Creation Date (UTC)",
+								value: `${formatDate(creationDate)} (${dateDelta} days ago)`,
+							},
+							{
+								name: "Age",
+								value: timeDiff(creationDate.getTime(), now),
+							},
+						],
+					}),
 				],
 			})
-		} catch (e) {
+		} catch {
 			message.channel.send({
 				embeds: [
-					new MessageEmbed()
-						.setTitle("Error")
-						.setDescription(`Failed to parse snowflake \`${input}\``),
+					new MessageEmbed({
+						title: "Error",
+						description: `Failed to parse snowflake \`${input}\``,
+					}),
 				],
 			})
 		}
