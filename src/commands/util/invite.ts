598842a231d0ec7566bee2911de338f54f6be51b
developomp <developomp@gmail.com>
2022-02-28 09:53:07 +0900

    added invite command

---
+++ src/commands/util/invite.ts
@@ -0,0 +1,20 @@
+import { Command, CommandOptions } from "@sapphire/framework"
+import { ApplyOptions } from "@sapphire/decorators"
+import { Message, MessageEmbed } from "discord.js"
+
+@ApplyOptions<CommandOptions>({
+	description: "Shows information about inviting the bot.",
+})
+export default class InviteCommand extends Command {
+	async messageRun(message: Message) {
+		message.channel.send({
+			embeds: [
+				new MessageEmbed({
+					title: "Sorry",
+					description: `Sorry, but only the owner can invite the Llama bot.
+Check the [documentation](https://docs.llama.developomp.com/docs/overview#can-i-use-this-bot-in-my-discord-server) for more information.`,
+				}),
+			],
+		})
+	}
+}
