8bd7ced7b97d9ddd7b89813730ced612c50ea9fa
developomp <developomp@gmail.com>
2022-03-03 16:31:27 +0900

    added docstring to `countDays.ts`

--- src/util/countDays.ts
+++ src/util/countDays.ts
@@ -1,3 +1,9 @@
+/**
+ * Counts the number of days in between {@link startDate} and {@link endDate}.
+ *
+ * @param {number} startDate - Starting date in milliseconds
+ * @param {number} endDate - Ending date in milliseconds
+ */
 export default function (startDate: number, endDate: number): number {
 	return Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24))
 }
