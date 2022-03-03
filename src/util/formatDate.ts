e1eccd8b0d4b4c38fb52e31fdfda86ce2565865a
developomp <developomp@gmail.com>
2022-03-03 21:08:49 +0900

    `formatDate` utility function update - added tests - fixed months incorrectly getting formatted

--- src/util/formatDate.ts
+++ src/util/formatDate.ts
@@ -4,5 +4,13 @@
  * @param {Date} date - Raw date object
  */
 export default function (date: Date): string {
-	return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+	const YYYY = date.getFullYear()
+	const MM = date.getMonth() + 1 // starts from 0 for some reason
+	const DD = date.getDate()
+
+	const hh = date.getHours()
+	const mm = date.getMinutes()
+	const ss = date.getSeconds()
+
+	return `${YYYY}-${MM}-${DD} ${hh}:${mm}:${ss}`
 }
