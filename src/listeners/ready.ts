3c2408569683f5aaea8fb24d9257d87b1de83225
developomp <developomp@gmail.com>
2022-03-05 23:17:35 +0900

    Simplified string templating

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -2,7 +2,7 @@ import { ApplyOptions } from "@sapphire/decorators"
 import { Listener } from "@sapphire/framework"
 import { gray, yellow } from "colorette"
 
-import type { ListenerOptions, Piece, Store } from "@sapphire/framework"
+import type { ListenerOptions } from "@sapphire/framework"
 
 import { globalObject } from ".."
 
@@ -21,32 +21,33 @@ export class Ready extends Listener {
 	printReady(): void {
 		// prints: botusername#discriminator (botuserid) is Ready!
 		console.log(
-			gray(
+			`${gray(
 				`${yellow(
 					this.container.client.user?.tag || "unknown bot name"
 				)} (ID: ${yellow(
 					this.container.client.user?.id || "unknown bot ID"
 				)}) is Ready!`
-			),
-			"\n"
+			)}
+`
 		)
 	}
 
 	printMode(): void {
 		console.log(
-			gray("Mode:"),
-			yellow(process.env.TESTING === "true" ? "DEVELOPMENT" : "PRODUCTION")
+			`${gray("Mode:")}  ${yellow(
+				process.env.TESTING === "true" ? "DEVELOPMENT" : "PRODUCTION"
+			)}`
 		)
 	}
 
 	printStoreDebugInformation(): void {
 		for (const store of this.container.client.stores.values())
-			console.log(this.styleStore(store))
-	}
-
-	styleStore(store: Store<Piece>): string {
-		return gray(
-			`Loaded ${yellow(store.size.toString().padEnd(3, " "))} ${store.name}.`
-		)
+			console.log(
+				gray(
+					`Loaded ${yellow(store.size.toString().padEnd(3, " "))} ${
+						store.name
+					}.`
+				)
+			)
 	}
 }
