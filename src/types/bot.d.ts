684dac4318e5321d74e59466a913cd0f57c3e417
developomp <developomp@gmail.com>
2022-03-07 23:24:54 +0900

    updated DB related files

--- src/types/bot.d.ts
+++ src/types/bot.d.ts
@@ -1,19 +1,27 @@
+export type Snowflake = string
+// structure: "<snowflake>/<snowflake>"
+export type MessageSelector = string
+
 export interface Settings {
+	// emoji names
 	clear_emojis?: string[]
+	// quotes to be used in the `llama` command
 	quotes?: string[]
 }
 
 export interface ServerData {
 	settings: {
-		enabledCogs: string[]
+		// todo: enable/disable commands by default
+		// todo: enabled/disabled commands/categories
 	}
+
 	vars: {
-		channels: string[]
-		messages: string[]
-		roles: string[]
+		channels: { [key: string]: Snowflake }
+		messages: { [key: string]: MessageSelector }
+		roles: { [key: string]: Snowflake }
 	}
 }
 
 export interface Servers {
-	[key: string]: ServerData
+	[key: Snowflake]: ServerData
 }
